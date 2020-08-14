d8dd1b8e0626bbc2df5a27c722978b2c2d2c5d12
travis skyles <travis.skyles@gmail.com>
2020-08-13 19:46:05 -0700

    news feed rss ticker working

---
+++ modules/news_rss/newsItem.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const NewsItem = class {
+  constructor(title, description){
+    this.title = title;
+    this.description = description;
+  }
+}
--- modules/news_rss/news_rss.js
+++ modules/news_rss/news_rss.js
@@ -2,16 +2,36 @@
 
 Module.register('news_rss', {
   _defaults: {
+    updateInterval: 1000 * 3600,
     url: 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+    type: 'scrolling',
   },
 
-  // _getStyles: function(){},
-  // _getScripts: function(){},
+  _getStyles: function(){
+    return [
+      'css/news_rss.css',
+    ]
+  },
+  _getScripts: function(){
+    return [
+      'newsItem.js',
+      dependencies['ejs'],
+    ]
+  },
   _getTemplateData: function(){
-    console.log('RSS CONFIG', this.data.config)
-    fetch(this.data.config.url)
+    const newsData = {
+      type: this.data.config.type,
+      data: [],
+    }
+    return fetch(this.data.config.url)
       .then(response => response.text())
       .then(string => new window.DOMParser().parseFromString(string, 'text/xml'))
-      .then(data => console.log(data))
+      .then(data => {
+        const items = data.querySelectorAll("item");
+        items.forEach(item => {
+          newsData.data.push(new NewsItem(item.children[0].textContent, item.children[4].textContent));
+        })
+        return { data: newsData };
+      })
   }
 })
